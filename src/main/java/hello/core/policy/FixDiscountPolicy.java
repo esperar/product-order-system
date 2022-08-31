03b502c329094e72270f4c5c70377d36a8379db4
KIMHUEMANG <piyrw9754@gmail.com>
2022-08-31 20:45:21 +0900

    주문과 할인 도메인 개발

---
+++ src/main/java/hello/core/policy/FixDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.core.policy;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy{
+
+    private int discountFixAmount = 1000;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return discountFixAmount; // vip면 1000원 할인
+        } else {
+            return 0;
+        }
+    }
+}
