35c9a786d32c3d79f31f02573cccac3417065a28
KIMHUEMANG <piyrw9754@gmail.com>
2022-09-01 20:59:47 +0900

    새로운 할인 정책 개발

---
+++ src/test/java/hello/core/policy/RateDiscountPolicyTest.java
@@ -0,0 +1,40 @@
+package hello.core.policy;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("고객 등급이 VIP 면 10퍼센트 할인")
+    void vip_o() {
+
+        //given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        Assertions.assertThat(discount).isEqualTo(1000);
+
+    }
+
+    @Test
+    @DisplayName("VIP가 아니라면 할인이 적용되지 않아야한다.")
+    void vip_x(){
+
+        //given
+        Member member = new Member(2L, "memberBASIC", Grade.BASIC);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        Assertions.assertThat(discount).isEqualTo(1000);
+    }
+
+}
