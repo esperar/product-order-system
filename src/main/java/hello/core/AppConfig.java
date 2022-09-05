3a6b3bec00d5e7c6412d1850c45507c861b69906
KIMHUEMANG <piyrw9754@gmail.com>
2022-09-05 20:58:04 +0900

    스프링으로 변환하기

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,21 +9,28 @@ import hello.core.order.OrderServiceImpl;
 import hello.core.policy.DiscountPolicy;
 import hello.core.policy.FixDiscountPolicy;
 import hello.core.policy.RateDiscountPolicy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberSerivce(){
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         // return new FixDiscountPolicy();
         return new RateDiscountPolicy();
