f173a3a9397ad7c8e82eab4419d45046eecb7c36
KIMHUEMANG <piyrw9754@gmail.com>
2022-09-18 00:21:17 +0900

    다양한 의존관계 주입 방법

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,11 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import hello.core.policy.DiscountPolicy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -10,4 +16,15 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
 
+    @Bean
+    OrderService orderService(MemberRepository memberRepository , DiscountPolicy discountPolicy){
+        return new OrderServiceImpl(memberRepository,discountPolicy);
+    }
+
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
 }
+
+
