140663d01e0c1c3101efb293b54d83e01478436a
KIMHUEMANG <piyrw9754@gmail.com>
2022-09-20 21:19:51 +0900

    Primary 주입 지정

--- src/main/java/hello/core/policy/RateDiscountPolicy.java
+++ src/main/java/hello/core/policy/RateDiscountPolicy.java
@@ -3,10 +3,11 @@ package hello.core.policy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("rateDiscountPolicy")
+@Primary
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
