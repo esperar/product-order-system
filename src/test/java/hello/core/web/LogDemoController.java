6d05d41d5095b6f10a10e711d4d33dcded76ff1c
KIMHUEMANG <piyrw9754@gmail.com>
2022-09-23 22:52:20 +0900

    myLogger Provider로 전환을 통한 오류 제거

--- src/test/java/hello/core/web/LogDemoController.java
+++ src/test/java/hello/core/web/LogDemoController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.inject.Provider;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
@@ -13,15 +14,17 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final Provider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
-    public String logDemo(HttpServletRequest request) {
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
         String requestURL = request.getRequestURI().toString();
+        MyLogger myLogger = myLoggerProvider.get();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
+        Thread.sleep(1000);
         logDemoService.logic("testId");
         return "OK";
     }
